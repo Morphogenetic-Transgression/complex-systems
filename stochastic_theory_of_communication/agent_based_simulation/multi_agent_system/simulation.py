180364ff1452c2386d6aa77bdbd8347345ddd8a0
upcore <ezakharian@external.enapter.com>
2025-02-15 18:08:20 +0400

    multi-cell: matplotlib animation developed

--- stochastic_theory_of_communication/agent_based_simulation/multi_agent_system/simulation.py
+++ stochastic_theory_of_communication/agent_based_simulation/multi_agent_system/simulation.py
@@ -7,12 +7,12 @@ class Simulation(Iterator):
     def __init__(self, group: MultiCell, steps_number: int):
         self.group = group
         self.steps_number = steps_number
-        
+        self.step_number = 0
+
     def __next__(self) -> np.ndarray:
         self.group.step()
-        return self.group.state
+        self.step_number += 1
+        return self.step_number
 
     def __iter__(self) -> Iterator:
         return self
-        
-        
--- stochastic_theory_of_communication/agent_based_simulation/multi_agent_system/system.py
+++ stochastic_theory_of_communication/agent_based_simulation/multi_agent_system/system.py
@@ -5,7 +5,7 @@ import numpy as np
 class Cell:
     ALPHABET_SIZE = 3
     NUMBER_OF_PARAMETERS = 3
-    STATE_SHAPE = (NUMBER_OF_PARAMETERS , )
+    STATE_SHAPE = (NUMBER_OF_PARAMETERS, )
     STATE_SPACE_SIZE = ALPHABET_SIZE ** NUMBER_OF_PARAMETERS
     # TODO automatic construction of STATE_SPACE in constructor
     STATE_SPACE = np.array([[0, 0, 0],
@@ -34,11 +34,11 @@ class Cell:
                             [2, 1, 2],
                             [2, 2, 0],
                             [2, 2, 1],
-                            [2, 2, 2]])
-    
-    def choice_random_state(): # from the univers (dependance on the cosmos)
+                            [2, 2, 2]]) / ALPHABET_SIZE
+
+    def choice_random_state():  # from the univers (dependance on the cosmos)
         return Cell.STATE_SPACE[np.random.choice(Cell.STATE_SPACE_SIZE)]
-        
+
 
 class MultiCell():
     def __init__(self, init_state: np.ndarray):
@@ -46,51 +46,53 @@ class MultiCell():
         (self.height, self.width, self.alphabet_size) = self.state.shape
         assert self.alphabet_size == Cell.ALPHABET_SIZE
         self.number_of_agent = self.height * self.width
-        self.__init_communicational_properties() # init_temperature, init_not_permitted_neighbors_table
-        
+        self.__init_communicational_properties()  # init_temperature, init_not_permitted_neighbors_table
+
     @property
     def state(self) -> np.ndarray:
         return self.__state
-    
+
     @property
     def temperature(self):
         return self.__temperature
-    
+
     def step_of_cell(self, cell_current_state, not_permitted_neighbors):
         cell_next_state = Cell.choice_random_state()
         if tuple(cell_next_state) not in not_permitted_neighbors:
-            assert tuple(cell_next_state) in [tuple(posible_state) for posible_state in Cell.STATE_SPACE] # TODO remove assert
+            assert tuple(cell_next_state) in [tuple(posible_state)
+                                              for posible_state in Cell.STATE_SPACE]  # TODO remove assert
             return cell_next_state
         return cell_current_state
-            
+
     def step(self) -> None:
         next_state = copy(self.state)
         for h in range(self.height):
             for w in range(self.width):
-                next_state[h][w] = self.step_of_cell(cell_current_state = next_state[h][w], # because it has been already copyed
-                                                     not_permitted_neighbors = self.__not_permitted_neighbors_table[h][w])
+                next_state[h][w] = self.step_of_cell(cell_current_state=next_state[h][w],  # because it has been already copyed
+                                                     not_permitted_neighbors=self.__not_permitted_neighbors_table[h][w])
         self.set_state(next_state)
 
     def set_state(self, state):
         self.__state = state
-        
+
     def __init_communicational_properties(self):
         # maybe here will be some additional initialisations
-        init_temperature = 0 # TODO add to arguments
+        init_temperature = 0  # TODO add to arguments
         self.set_temperature(temperature=init_temperature)
-        self.__not_permitted_neighbors_table = [] # TODO add init table to arguments (?)
+        self.__not_permitted_neighbors_table = []  # TODO add init table to arguments (?)
         for h in range(self.height):
             not_permitted_neighbors_row = []
             for w in range(self.width):
                 not_permitted_neighbors_row.append([])
             self.__not_permitted_neighbors_table.append(not_permitted_neighbors_row)
-        
+
     def set_temperature(self, temperature):
-        self.__temperature = min(max(temperature, 0), Cell.STATE_SPACE_SIZE)    
+        temperature = int(temperature)
+        temperature = max(temperature, 0)
+        self.__temperature = min(temperature, Cell.STATE_SPACE_SIZE)
 
     def set_communication_rules(self):
         for h in range(self.height):
             for w in range(self.width):
-                self.__not_permitted_neighbors_table[h][w] = [tuple(not_permitted_state) 
+                self.__not_permitted_neighbors_table[h][w] = [tuple(not_permitted_state)
                                                               for not_permitted_state in Cell.STATE_SPACE[:self.__temperature]]
-    
